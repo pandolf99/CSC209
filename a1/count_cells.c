3f29c5b3d82f2070905b014d2630ef7442dae65c
Acostaig <ignacio.acosta@mail.utoronto.ca>
2019-09-28 16:11:49 -0400

    Can load file into array

--- a1/count_cells.c
+++ a1/count_cells.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void read_image(int num_rows, int num_cols, 
+void read_image(int num_rows, int num_cols,
                 int arr[num_rows][num_cols], FILE *fp);
-                
+
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
 // Remember to include the function prototype for count_cells
@@ -13,6 +14,23 @@ int main(int argc, char **argv) {
     // Print a message to stderr and exit with an argument of 1 if there are
     // not the right number of parameters, or the second argument is not -p
 
+    if (((argc - 1) < 1) || ((argc - 1) > 2) ||
+    (argc == 3 && strcmp(argv[2], "-p") != 0)) {
+      fprintf(stderr, "Usage: count_cells <imagefile.txt> [-p] \n");
+      return 1;
+    }
 
+    // Declare important variables and get size
+    FILE *f = fopen(argv[1], "r");
+    int height;
+    int width;
+    fscanf(f, "%d %d\n", &height, &width);
+    int arr[height][width];
+
+    read_image(height, width, arr, f);
+    // Check for second parameter
+    if (argc == 3 && strcmp(argv[2], "-p") == 0) {
+      print_image(height, width, arr);
+    }
     return 0;
 }
--- a1/image.c
+++ a1/image.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
-    
-/* Reads the image from the open file fp into the two-dimensional array arr 
+#include <stdlib.h>
+
+/* Reads the image from the open file fp into the two-dimensional array arr
  * num_rows and num_cols specify the dimensions of arr
  */
-void read_image(int num_rows, int num_cols, 
+
+int get_val_at_i(int i, char *str) {
+  if (i == 0) {
+    return strtol(str, NULL, 10);
+  }
+  else {
+    char *other = NULL;
+    strtol(str, &other, 10);
+    return get_val_at_i(i-1, other);
+  }
+}
+
+void read_image(int num_rows, int num_cols,
                 int arr[num_rows][num_cols], FILE *fp) {
 
+                  for (int i = 0; i < num_rows; i++) {
+                    // Max size of line is if all numbers are of 3 digits
+                    char *line = malloc(sizeof(char)*(2*num_cols*3));
+                    fscanf(fp, "%[^\n]\n", line);
+                    for (int i2 = 0; i2 < num_cols; i2++) {
+                      arr[i][i2] = get_val_at_i(i2, line);
+                    }
+                    free(line);
+                  }
 }
 
 /* Print to standard output the contents of the array arr */
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
-
+  for (int i = 0; i < num_rows; i++) {
+    for (int i2 = 0; i2 < num_cols; i2++) {
+      printf(" %d ", arr[i][i2]);
+    }
+    printf("\n");
+  }
 }
 
 /* TODO: Write the count_cells function */
