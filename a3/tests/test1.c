81c6d08a63db97b1210c42e4e1b66f792fdb46e4
Acostaig <ignacio.acosta@mail.utoronto.ca>
2019-11-13 11:50:28 -0500

    pushing test folder

---
+++ a3/tests/test1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+
+int main() {
+  printf("%d\n", MAXLINE);
+  return 0;
+}
---
+++ a3/tests/test2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+
+int main() {
+  printf("2: %d", MAXLINE);
+  return 0;
+}
